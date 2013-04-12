e746888c4759114b7ebfa1f10324726cda730eae
CloudDeLuna <herbaut.robin@gmail.com>
2013-04-12 23:03:55 +0200

    ajout de tests

--- src/test/java/fr/univaix/iut/pokebattle/bot/BotRunnerTest.java
+++ src/test/java/fr/univaix/iut/pokebattle/bot/BotRunnerTest.java
@@ -3,18 +3,12 @@ package fr.univaix.iut.pokebattle.bot;
 import org.junit.Test;
 
 import fr.univaix.iut.pokebattle.run.BotRunner;
-import fr.univaix.iut.pokebattle.run.PokemonMain;
 
 public class BotRunnerTest {
 
 	@Test
 	public void test() {
 		BotRunner.runBot(new PokeBot(), "twitter4j-Smogogo.properties");
-	}
-	
-	@Test
-	public void tests() {
-		PokemonMain.main(null);
 	}
 
 }
